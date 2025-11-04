bd4dbda2a17702070610e76a6edb9c2ab7e87129
MohmmadSadraBagherpoor <bagherpoorsadra@gmail.com>
2025-11-04 09:52:06 +0330

    feat:Add Zibal Gateway

---
+++ src/Zibal/Zibal.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace Larabookir\Gateway\Zibal;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Request;
+use Larabookir\Gateway\Enum;
+use Larabookir\Gateway\PortAbstract;
+use Larabookir\Gateway\PortInterface;
+use Larabookir\Gateway\Zibal\ZibalException;
+
+class Zibal extends PortAbstract implements PortInterface
+{
+    const SERVER_URL = 'https://gateway.zibal.ir';
+    const REQUEST_URL = '/v1/request';
+    const START_URL = '/start/';
+    const VERIFY_URL = '/v1/verify';
+    const INQUIRY_URL = '/v1/inquiry';
+    protected $paymentUrl;
+    protected $settings;
+    protected $invoiceNumber;
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $this->settings = (object) Config::get('gateway.Zibal');
+    }
+    
+    public function set($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function ready()
+    {
+        $this->sendPayRequest();
+        return $this;
+    }
+
+    public function redirect()
+    {
+        return \Redirect::to($this->getGatewayUrl());
+    }
+
+    public function verify($transaction)
+    {
+        parent::verify($transaction);
+        $this->userVerify();
+        $this->verifyPayment();
+        return $this;
+    }
+    
+    public function setInvoiceNumber($invoiceNumber)
+    {
+        $this->invoiceNumber = $invoiceNumber;
+    }
+
+    public function getInvoiceNumber()
+    {
+        return $this->invoiceNumber;
+    }
+
+    function setCallback($url)
+    {
+        $this->callbackUrl = $url;
+        return $this;
+    }
+
+    function getCallback()
+    {
+        if (!$this->callbackUrl) {
+            $this->callbackUrl = $this->config->get('gateway.Zibal.callbackUrl');
+        }
+
+        return $this->makeCallback($this->callbackUrl, ['transaction_id' => $this->transactionId()]);
+    }
+    
+    private function request()
+    {
+        $url = self::SERVER_URL . self::REQUEST_URL;
+        
+        $params = [
+            'merchant'      => $this->config->get('gateway.Zibal.merchant'),
+            'amount'        => $this->amount,
+            'cellNumber'    => $this->mobileNumber,
+            'orderId'       => $this->transactionId,
+            'callbackUrl'   => $this->getCallback(),
+        ];
+        
+        $headers = [
+            'Agent' => 'WEB',
+            'Content-Type' => 'application/json',
+        ];
+        
+        $response = \Http::withHeaders($headers)
+            ->post($url, $params);
+        
+        $body = json_decode($response->body(), true);
+        
+        if ($response->status() != 200 && $body['result'] != 100) {
+            $message = $body['message'] ?? 'خطا در هنگام درخواست برای پرداخت رخ داده است.';
+            throw new ZibalException($body['result']);
+        }
+        
+        return $body['trackId'];
+    }
+
+    protected function sendPayRequest()
+    {
+        $this->newTransaction();
+        $trackId = $this->request();
+        $url = self::SERVER_URL . self::START_URL . $trackId;
+        $this->setPaymentUrl($url);
+    }
+
+    protected function commit()
+    {
+        // No use
+    }
+
+    protected function verifyPayment()
+    {
+        $this->refId = \Request::input('trackId');
+
+        $url = self::SERVER_URL . self::VERIFY_URL;
+        
+        $params = [
+            'trackId'       => $this->refId,
+            'merchant'      => $this->config->get('gateway.Zibal.merchant'),
+        ];
+        
+        $response = \Http::withHeaders([
+            'Content-Type'  => 'application/json',
+        ])->post($url, $params);
+        
+        $body = json_decode($response->body(), true);
+        
+        if ($response->status() != 200
+            && $body['result']  != 100
+            && $body['message'] != 'success'
+            && $body['amount']  != $this->amount
+        ) {
+            $message = $body['result']['message'] ?? 'تراکنش تایید نشد';
+            throw new ZibalException($body['result']);
+        }
+        $this->transactionSucceed();
+        $this->newLog($response->status(), Enum::TRANSACTION_SUCCEED_TEXT);
+    }
+    
+    protected function userVerify()
+    {
+        $status = \Request::get('status');
+        if ($status == 1 || $status == 2){
+            return true;
+        }
+        $this->transactionFailed();
+        throw new ZibalException($status);
+    }
+    
+    public function getGatewayUrl()
+    {
+        return $this->paymentUrl;
+    }
+
+    private function setPaymentUrl(string $paymentUrl)
+    {
+        $this->paymentUrl = $paymentUrl;
+
+        return $this;
+    }
+    
+    protected function transactionSucceed()
+    {
+        return $this->getTable()->whereId($this->transactionId)->update([
+            'ref_id'        => $this->refId,
+            'payment_date'  => \Carbon::now(),
+            'updated_at'    => \Carbon::now(),
+            'card_number'   => $this->cardNumber,
+            'tracking_code' => $this->trackingCode,
+            'status'        => Enum::TRANSACTION_SUCCEED,
+        ]);
+    }
+}
+
+
